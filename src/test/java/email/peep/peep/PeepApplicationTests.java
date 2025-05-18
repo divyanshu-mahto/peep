26e02fd3c0481dfef36fa337e7bfe1f988a1c18b
divyanshu-mahto <divyanshukm17@gmail.com>
2025-05-18 15:32:36 +0530

    Initial commit

---
+++ src/test/java/email/peep/peep/PeepApplicationTests.java
@@ -0,0 +1,13 @@
+package email.peep.peep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
